b2e2852b70a3bcf6f84461ea35cd92db7f9673f8
Aneeta Paulose <89934094+AneetaP123@users.noreply.github.com>
2026-03-23 11:39:56 +0100

    Update 4_16decoder_tb.v

--- decoder/4_16decoder_tb.v
+++ decoder/4_16decoder_tb.v
@@ -4,26 +4,26 @@ module tb_decoder4to16;
 
     // Testbench signals
     reg A3,A2, A1, A0;
-    wire [7:0] Y;
+  wire [15:0] Y;
 
     // Instantiate the decoder
     decoder4to16 uut (
-      	.A3(A3)
+        .A3(A3),
         .A2(A2),
         .A1(A1),
         .A0(A0),
         .Y(Y)
     );
-
+	integer i;
     // Stimulus
     initial begin
-      $display("Time\t A3 A2 A1 A0 | Y15 Y14 Y13 Y12 Y11 Y10 Y9 Y8 Y7 Y6 Y5 Y4 Y3 Y2 Y1 Y0");
+      	$display("Time\t A3 A2 A1 A0 | Y15 Y14 Y13 Y12 Y11 Y10 Y9 Y8 Y7 Y6 Y5 Y4 Y3 Y2 Y1 Y0");
         $display("---------------------------------------------");
 
         // Loop through all input combinations
-        integer i;
+      
       for (i = 0; i < 16; i = i + 1) begin
-          {A3,A2, A1, A0} = i; #10;
+        {A3, A2, A1, A0} = i; #10;
           $display("%0t\t %b %b %b  %b | %b", $time, A3, A2, A1, A0, Y);
         end
 
